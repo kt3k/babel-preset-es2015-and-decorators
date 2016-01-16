c9a9fd1fa567ff5938ccd8ddac8665e96dc810ef
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-16 20:55:33 +0900

    Add decorators transform

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require("babel-plugin-syntax-decorators"),
+    require("babel-plugin-transform-decorators"),
     require("babel-plugin-transform-es2015-template-literals"),
     require("babel-plugin-transform-es2015-literals"),
     require("babel-plugin-transform-es2015-function-name"),

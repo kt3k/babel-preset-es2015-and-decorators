973c1ecb6217c25af85d3e70af596c429b487234
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-16 21:05:00 +0900

    Switch to 3rd party's

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
-    require("babel-plugin-syntax-decorators"),
-    require("babel-plugin-transform-decorators"),
+    require("babel-plugin-transform-decorators-legacy"),
     require("babel-plugin-transform-es2015-template-literals"),
     require("babel-plugin-transform-es2015-literals"),
     require("babel-plugin-transform-es2015-function-name"),
